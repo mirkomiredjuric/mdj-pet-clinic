bbf7cd7993f85ddf97076cd5c485ebd4efd8e902
Mirko Djuric <mirkomiredjuric@gmail.com>
2021-02-01 21:29:42 +0100

    Inital commit

---
+++ src/main/java/mire/springframework/mdjpetclinic/MdjPetClinicApplication.java
@@ -0,0 +1,16 @@
+package mire.springframework.mdjpetclinic;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MdjPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdjPetClinicApplication.class, args);
+       
+    }
+
+}
---
+++ src/test/java/mire/springframework/mdjpetclinic/MdjPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package mire.springframework.mdjpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdjPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
